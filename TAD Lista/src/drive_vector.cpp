d2656b26a2e7459904aa3d4235d20927864c80fc
Thiago Cesar <thiagoc.lucena@gmail.com>
2016-05-18 01:36:45 +0000

    drive_frwd_list.cpp adicionado

--- TAD Lista/src/drive_vector.cpp
+++ TAD Lista/src/drive_vector.cpp
@@ -7,10 +7,6 @@ int main()
 {
     Vector<int> vec;
     
-    Forward_List<int> forw; 
-    
-    /*
-    
     std::cout << ">>>" << vec.capacity() << "\n";
     vec.push_back(3);
     std::cout << ">>>" << vec.capacity() << "\n";
@@ -37,28 +33,6 @@ int main()
     }
     
     std::cout << ">>>" << vec.capacity() << "\n";
-    
-    */
-    
-    if ( forw.empty() )
-        std::cout << "Empty!\n";
-    else
-        std::cout << "NOT empty!\n";
-        
-    forw.push_back(7);    
-    forw.push_front(8);
-    forw.push_back(9);
-    forw.push_front(3);
-    forw.push_front(4);
-    forw.push_back(56);
-    forw.pop_front();
-    forw.pop_back();
-    forw.assign(4);
 
-    std::cout << "Trás: >>>" << forw.back() << "\n";
-    std::cout << "Frente: >>>" << forw.front() << "\n";
-    std::cout << "Tamanho: >>>" << forw.size() << "\n";
-
-        
     return EXIT_SUCCESS;
 }
